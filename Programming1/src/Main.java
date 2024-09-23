172ad90d586fb04a3390de8c6f52eb238811b088
jay95131 <jayadeep.mandyam@csuglobal.edu>
2024-09-22 23:10:32 -0700

    Add files via upload
    Initial commit

---
+++ Programming1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Smith";
+        String streetAddress = "123 First St";
+        String city = "Anycity";
+        String zipCode = "12345";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
